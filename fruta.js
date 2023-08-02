86939e4970925c909eab16a03ea61c06372860f3
Flavio Silva <fal_silva@hotmail.com>
2023-08-01 23:38:51 -0300

    htmal e js alterados

--- fruta.js
+++ fruta.js
@@ -1,22 +1,29 @@
 function Calcular() {
   let frutas1 = document.querySelector('#ifrutas1')
-  let optionValue = frutas1.options[frutas1.selectedIndex]
-  let quantidade = document.getElementById('iqtd4')
-  let geral = optionValue.value
-  // console.log(value);
+  let geral = frutas1.value
+  
   
   //Pegando o valor do select e passando para o input text
   //preço da fruta = geral
  document.getElementById('ires1').value = geral
-    console.log(geral)
+ //console.log(geral)
+
+  //Obtem o valor do input
+  let quantidade = document.getElementById('iqtd4')
+  let valorInput = quantidade.value 
+  //console.log(valorInput) 
    
 
-  let total = geral * quantidade
-  console.log(total)
+  let total = geral * valorInput
+  //console.log(total)
+
+  // Exibe o valor calculado no input de resultado
+  let resultadoInput = document.getElementById('ires3');
+  resultadoInput.value = total;
    
 }
 
-//Calcular()
+Calcular()
 
 
 
